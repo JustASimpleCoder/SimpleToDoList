f49ce8dd732152a7c5ebf9e693e4bd3c0f68dbaf
Jacob Cohen <thejacobcohen@gmail.com>
2024-10-29 15:00:37 -0400

    Initial program set up

--- main.cpp
+++ main.cpp
@@ -1,4 +1,26 @@
-int main(int argc, char* argv[])) {
+#include <iostream> 
+#include <vector>  
+#include <string>  
 
+
+
+int main(int argc, char* argv[]) {
+   
+    std::cout << "Currently list fo tasks [" << 5  << "] " << std::endl;
+
+    std::string currentTask = "";
+    std::cout << "Would you like to Add (A), Delete (D) or Mark Complete (M), or list all tasks (L) " << std::endl;
+
+    while(true){
+        while(currentTask != "A" || currentTask != "D" || currentTask != "M" ){
+            std::cin >> currentTask;
+            if(currentTask != "A" || currentTask != "D" || currentTask != "M"){
+                std::cout << "please enter a valid task (A, D, or M)";
+            }
+        }
+    }
+    
     return 0;
 }
+
+
