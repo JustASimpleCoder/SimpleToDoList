5fb7ff40060ba4d323e2111332dc51fdb994da91
Jacob Cohen <thejacobcohen@gmail.com>
2024-10-30 14:09:09 -0400

    Add memory via file handling to store tasks

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # SimpleToDoList
 Simple To Do List that is handeled only through a CLI
 
-These files are to explore how to teach basic C++ concept with a simple project
+These files are to explore how to teach basic C++ concepts (Syntax, Functions, I/O, and file handling) with a simple project. 
+
+This is a simple task list program that you can intergace with the CLI. You have a few simple commands Add (A), Delete (D), Mark Complete (M), list all tasks (L) or Quit Program (Q). 
--- main.cpp
+++ main.cpp
@@ -2,7 +2,38 @@
 #include <vector>  
 #include <string>  
 
+#include <fstream>
+
 std::vector<std::string> taskList = {};
+
+
+
+void writeTaskList(){
+    std::ofstream file("TaskList.txt"); // Open the file in write mode
+    if (file.is_open()) {
+        for(auto &task:taskList){
+            file << task + "\n";
+        }
+        file.close(); // Close the file after writing
+    } else {
+        std::cerr << "Unable to open file for writing.\n";
+    }
+
+}
+void readAndGenerateTaskList(){
+    std::ifstream file("TaskList.txt"); // Open the file in read mode
+    std::string line;
+    if (file.is_open()) {
+        while (std::getline(file, line)) { 
+            taskList.push_back(line); 
+        }
+        file.close(); 
+    } else {
+        std::cerr << "Unable to open file for reading.\n";
+    }
+}
+
+
 bool addTask(std::string task){
     if(task != ""){
         int taskNum = taskList.size() + 1;
@@ -11,18 +42,18 @@ bool addTask(std::string task){
         std::cout << "Successfully added new task [" << task << "] to the list \n";
         return true;
     }else{
-        std::cout << "Could not add the new task as it had no value. Please enter a task with characters \n";
+        std::cout << "Could not add the new task as it was an empty string. Please enter a task with characters \n";
         return false;
     }
 }
 
 bool deleteTask(int taskNumber){
     if(taskNumber <= 0 || taskNumber > taskList.size()){
-        std::cout << "Please enter a valid task number from 1 to the number of tasks ( " << taskList.size() << ")" << std::endl;
+        std::cout << "Please enter a valid task number from 1 to the number of tasks ( " << taskList.size() << ") \n" << std::endl;
         return false;
     }else{
         taskList.erase(taskList.begin() + taskNumber - 1);
-        std::cout << "Deleted task [" << taskList.size() << "] from the list" << std::endl;
+        std::cout << "Deleted task [" << taskList.size() << "] from the list \n" << std::endl;
         return true;
     }
 }
@@ -38,12 +69,12 @@ bool markCompleteTask(int taskNumber){
     if(foundIndex != std::string::npos & foundIndex > 0){
         std::string completedTask = "";
         completedTask = task.substr(0, foundIndex);
-        std::cout << "marked task as completed [" << completedTask << "] \n" << std::endl;
+        std::cout << "Marked task as completed [" << completedTask << "] \n" << std::endl;
         completedTask += "(Completed)";
         taskList[taskNumber - 1] = completedTask;
         return true;
     }else{
-        std::cout << "Task number " << taskNumber << ". is already marked complete";
+        std::cout << "Task number " << taskNumber << ". is already marked complete \n";
         return false;
     }
 }
@@ -57,6 +88,7 @@ void listAllTask(){
 }
 
 int main(int argc, char* argv[]) {
+    readAndGenerateTaskList();
     while(true){
         std::string currentTask = "";
         std::cout << "Would you like to Add (A), Delete (D), Mark Complete (M), list all tasks (L) or Quit Program (Q)" << std::endl;
@@ -66,10 +98,11 @@ int main(int argc, char* argv[]) {
             std::cout << "Please enter a valid task (A, D, M, L or Q)";
             std::cin >> currentTask;
         }
-        std::cout << "You have entered the task [" << currentTask  << "] \n" << std::endl;
 
+        std::cout << "You have entered the task [" << currentTask  << "] \n" << std::endl;
         if(currentTask == "Q"){
             std::cout << "Quitting Program \n" << std::endl;
+            writeTaskList();
             return 0;
         }
 
@@ -106,10 +139,10 @@ int main(int argc, char* argv[]) {
                 if (std::cin.fail()) {
                     std::cin.clear();
                     // std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                    std::cout << "Invalid input. Please enter an integer.\n";
+                    std::cout << "Invalid input. Please enter an integer. \n";
                     taskNum = -1;
                 }
-                
+
             }while(!markCompleteTask(taskNum));
         }
 
